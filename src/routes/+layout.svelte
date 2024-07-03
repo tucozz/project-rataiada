1458ed26a1477b32eb1fb73dedfeab04df220674
Jheam Storch Ross <58143641+srjheam@users.noreply.github.com>
2024-07-03 04:09:01 +0000

    feat: Add PWA support and CI pipeline

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,43 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
+	import { onMount } from 'svelte';
+	import { pwaInfo } from 'virtual:pwa-info';
+	import { pwaAssetsHead } from 'virtual:pwa-assets/head';
+
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					// uncomment following code if you want check for updates
+					r && setInterval(() => {
+					   console.log('Checking for sw update')
+					   r.update()
+					}, 20000 /* 20s for testing purposes */)
+					//console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
+
+<svelte:head>
+	{#if pwaAssetsHead.themeColor}
+		<meta name="theme-color" content={pwaAssetsHead.themeColor.content} />
+	{/if}
+	{#each pwaAssetsHead.links as link}
+		<link {...link} />
+	{/each}
+
+	{@html webManifestLink}
+</svelte:head>
 
 <div class="app">
 	<Header />
@@ -14,6 +50,10 @@
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 </div>
+
+{#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt}}
+  <ReloadPrompt />
+{/await}
 
 <style>
 	.app {
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			pwaAssets: {
+				config: true,
+			},
+			manifest: {
+				short_name: 'rataiada',
+				name: 'Rataiada',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				theme_color: "#ffffff",
+				background_color: "#ffffff"
+			},
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
