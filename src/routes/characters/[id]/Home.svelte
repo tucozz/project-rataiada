8c97cff80badf0e5f43b7185035e693658a9c2f9
Jheam Storch Ross <58143641+srjheam@users.noreply.github.com>
2024-07-09 20:36:07 +0000

    sdfdffddfds

--- src/routes/characters/[id]/Home.svelte
+++ src/routes/characters/[id]/Home.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { CharacterIdentity } from '$lib/components/routes/characters/[id]/_pages/home/character-identity';
+	import { Skeleton } from '$lib/components/ui/skeleton';
 	import type { Character } from '$lib/database';
 	import type { load } from './+page';
 
@@ -7,6 +8,13 @@
 	export let character: Character;
 </script>
 
-<div>
+<div class="flex flex-col gap-16">
 	<CharacterIdentity data={data.identityForm} {character} />
+
+	<!-- TODO: add portrait -->
+	<Skeleton class="h-96" />
+
+	<!-- TODO: add attributes -->
+
+	<!-- TODO: add grit -->
 </div>
